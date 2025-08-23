e4caeab8771b2b75b0ee43f4e1702dbe68d5582b
rahul022022 <codewithrahul22@gmail.com>
2025-08-23 21:59:05 +0530

    23-08-2025

---
+++ 05_user_input/02_add.py
@@ -0,0 +1,12 @@
+# a = input("Enter first number")
+# a = int(a)
+
+
+# print(a+3)
+
+a = int(input("enter first number"))
+# a = int(a)
+b = int(input("enter second number"))
+# b = int(b)
+
+print(a+b)
